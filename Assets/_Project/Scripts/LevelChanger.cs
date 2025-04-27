278c3bcccdada16c2a6b9181dd8af9ef9b633e5d
rajipkanxo01 <pdlrajip12@gmail.com>
2025-04-27 21:49:15 +0200

    Added level changer script

---
+++ Assets/_Project/Scripts/LevelChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts
+{
+    public class LevelChanger : MonoBehaviour
+    {
+        [SerializeField] private string _nextLevelName;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                LoadNextLevel();
+            }
+        }
+
+        private void LoadNextLevel()
+        {
+            if (!string.IsNullOrEmpty(_nextLevelName))
+            {
+                SceneManager.LoadScene(_nextLevelName);
+            }
+            else
+            {
+                Debug.LogWarning("Next Level Name not set in LevelChanger script!");
+            }
+        }
+    }
+}
